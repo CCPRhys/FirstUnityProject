ec36d950a37785a2532c389f5ac7b4815a2c5b54
Rhys <160487424+CCPRhys@users.noreply.github.com>
2025-12-11 15:27:57 +0000

    Added complicated turning

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,11 +14,41 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        controller.Move(lastMovementInput * Time.deltaTime);
+        controller.Move(new Vector3(lastMovementInput.x, 0, lastMovementInput.y) * Time.deltaTime);
     }
 
     public void MovementInput(InputAction.CallbackContext context)
     {
         lastMovementInput = context.ReadValue<Vector2>();
     }
+
+    public void MouseInput(InputAction.CallbackContext context)
+    {
+        Debug.Log("Mouse position:" + context.ReadValue<Vector2>());
+
+        Ray ray = Camera.main.ScreenPointToRay(context.ReadValue<Vector2>());
+        RaycastHit hit = new RaycastHit();
+
+        if(Physics.Raycast(ray, out hit, 100f))
+        {
+
+            Vector2 hitPosition;
+            hitPosition.x = hit.point.x;
+            hitPosition.y = hit.point.z;
+
+            Vector2 playerPosition;
+            playerPosition.x = this.transform.position.x;
+            playerPosition.y = this.transform.position.z;
+
+
+            Vector2 direction = hitPosition - playerPosition;
+            direction.Normalize();  
+            float rotationDirection = Mathf.Atan2(direction.x, direction.y);
+            Debug.Log("Looking direction: " + rotationDirection);
+
+            Quaternion rot = Quaternion.Euler(new Vector3(0, rotationDirection * Mathf.Rad2Deg, 0));
+            this.transform.rotation = rot;
+        }
+    }
+
 }
